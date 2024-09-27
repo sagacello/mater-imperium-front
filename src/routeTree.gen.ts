c6707df2689370b12cb7f3c4be0fbe97a61ee201
Marcelo Viola <marcelo.viola@ringa.com.br>
2024-09-27 16:46:58 -0300

    add route reference to render the same page

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,12 +8,23 @@
 // Import Routes
 import { Route as rootRoute } from "./routes/__root";
 import { Route as DashboardRoute } from "./routes/dashboard";
+import { Route } from "@tanstack/react-router";
+import { SystemsDashboard } from "./pages/SystemsDashboard";
 
 const DashboardPathRoute = DashboardRoute.update({
   path: "/dashboard",
   getParentRoute: () => rootRoute,
 });
 
-export const routeTree = rootRoute.addChildren([DashboardPathRoute]);
+const RootPathRoute = new Route({
+  path: "/",
+  component: SystemsDashboard,
+  getParentRoute: () => rootRoute,
+});
+
+export const routeTree = rootRoute.addChildren([
+  DashboardPathRoute,
+  RootPathRoute,
+]);
 
 /* prettier-ignore-end */
